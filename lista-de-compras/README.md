e73a959f1360fa38b742d5bc1c504bcc99eac9aa
NicolasMCP <nicolas.mcp@gmail.com>
2020-11-19 15:03:09 -0300

    incluindo rodapé

--- lista-de-compras/README.md
+++ lista-de-compras/README.md
@@ -1,15 +1,18 @@
 # PROJETO EM MUDANÇAS ATÉ SEXTA-FEIRA... 
 # POR FAVOR AGUARDE...!!
 
+
 # Lista de Compras
 
 ![license1](https://img.shields.io/static/v1?label=License&message=MIT&color=orange)
 
 O projeto (lista-de-compras) foi gerado com  [Angular CLI](https://github.com/angular/angular-cli) versão 10.2.0.
 
+
 ## Servidor de desenvolvimento
 
 Executando o comando `ng serve -o` para um servidor de desenvolvimento. O navegador será aberto automaticamente em `http://localhost:4200/`. O aplicativo será recarregado automaticamente se você alterar qualquer um dos arquivos.
+
 
 ## Componentes Angular Material
 
@@ -24,14 +27,22 @@ Executando o comando `ng serve -o` para um servidor de desenvolvimento. O navega
     - Um mat-icon usado dentro do header, antes do título
 - [Button](https://v10.material.angular.io/components/button/overview)
     - Um mat-raised-button usado ainda dentro do header, do lado direito
+- [Card](https://v10.material.angular.io/components/card/overview)
+    - Um mat-card usado como footer (rodapé)
+
 
 ## Meus componentes
 
 - home
+
 - compras-list
+
 - footer
 
+
 ## Comunicação
+
+- Entre o Button (dentro do Toolbar) e a lista de compras e os dados são atualizados nos Tabs
 
 
 
--- lista-de-compras/src/app/app.module.ts
+++ lista-de-compras/src/app/app.module.ts
@@ -5,18 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { ComprasListComponent } from './views/home/compras-list/compras-list.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatDialogModule} from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCardModule } from '@angular/material/card';
+import { FooterComponent } from './views/home/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ComprasListComponent
+    ComprasListComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -26,7 +29,8 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatIconModule,
     MatTabsModule,
     MatDialogModule,
-    MatButtonModule
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
